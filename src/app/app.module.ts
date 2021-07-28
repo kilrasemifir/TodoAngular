4166f335ea5cfb6dd6f8af414f3d1066eb20fd6d
Killian Raoux <killian@semifir.com>
2021-07-28 09:57:34 +0200

    version sans routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoItemComponent } from './components/todo-item/todo-item.component';
 import { TodoFormulaireComponent } from './components/todo-formulaire/todo-formulaire.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoItemComponent,
-    TodoFormulaireComponent
+    TodoFormulaireComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
